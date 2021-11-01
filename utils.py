14fe2f240eebb0d4b6d51f9d9bcf820828d2358d
Alessio Arena <alessio.arena@csiro.au>
2021-11-01 18:08:19 +1000

    improve/add conversions, add sample_cmap

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import matplotlib.colors as col
+import matplotlib.cm as cm
 from IPython.core.display import HTML, display
 import numpy as np
 from itertools import cycle, islice
@@ -13,7 +14,7 @@ def print_color(color):
         hex codes to be printed in your jupyter notebook
     """
     out = ""
-    if isinstance(color, list):
+    if isinstance(color, (list, np.ndarray)):
         for c in color:
             out += "<b style='color: {0}'>{0}</b>   ".format(c)
     else:
@@ -38,6 +39,65 @@ def hex_to_rgb(hex_code, normalized=True):
     if normalized:
         return tuple(float(int(h[i:i+2], 16))/255 for i in (0, 2 ,4))
     return tuple(int(h[i:i+2], 16) for i in (0, 2 ,4))
+
+
+def rgb_to_hex(rgb_tuple, normalized=True):
+    """Utility to convert RGB or RGBA tuples to HEX color strings
+
+    Parameters
+    ----------
+    rgb_tuple : 3- or 4-items tuple
+        RGB or RGBA code
+    normalized : bool, optional
+        Whether the RGB code is expressed as [0,1] or [0,255], by default True
+
+    Returns
+    -------
+    str
+        HEX string
+    """
+
+    if not isinstance(rgb_tuple, (tuple, list, np.ndarray)) or not (len(rgb_tuple) == 3 or len(rgb_tuple) == 4):
+        raise ValueError("you must provide a tuple of 3 elements")
+    r = []
+    for channel in rgb_tuple[:3]:
+        if normalized:
+            r.append(int(channel * 255))
+        else:
+            if channel < 0 or channel > 255:
+                raise ValueError('Invalid RGB value encountered. Make sure than this represents a 8bit code')
+            r.append(channel)
+    return '#%02x%02x%02x' % tuple(r)
+
+def cmap_to_hex(cmap):
+    """Utility to convert a matplotlib.colors.Colormap to a list of HEX strings
+
+    Parameters
+    ----------
+    cmap : matplotlib.colors.Colormap
+        Colormap object
+
+    Returns
+    -------
+    list of str
+        list of HEX strings
+    """
+    return list(map(rgb_to_hex, cmap.colors))
+
+def cmap_to_rgb(cmap):
+    """Utility to convert a matplotlib.colors.Colormap to a list of RGB codes
+
+    Parameters
+    ----------
+    cmap : matplotlib.colors.Colormap
+        Colormap object
+
+    Returns
+    -------
+    list of lists of floats
+        RGB tuples
+    """
+    return [c[:3] for c in cmap.colors]
 
 
 def generate_discrete_cmap(color_list):
@@ -100,3 +160,58 @@ def generate_linear_cmap(color_list, name='CSIRO'):
         cdict[channel] = tuple(steps)
     return col.LinearSegmentedColormap(name, cdict)
 
+
+def randomize_cmap(cmap, n=None):
+    """Utility to randomize a Colormap
+
+    Parameters
+    ----------
+    cmap : matplotlib.colors.Colormap
+        Colormap to randomize
+    n : int, optional
+        number of colors to sample, by default None
+
+    Returns
+    -------
+    matplotlib.colors.ListedColormap
+        discrete colormap with random sampled colors
+
+    """
+    if isinstance(cmap, str):
+        cmap = cm.get_cmap(cmap)
+    elif isinstance(cmap, col.Colormap):
+        pass
+    else:
+        raise TypeError('The argument "cmap" must be a string or Colormap')
+    if n is None:
+        n = cmap.N
+    vals = np.arange(0,cmap.N,1)
+    np.random.shuffle(vals)
+    cmap = col.ListedColormap(cmap(vals[:n]))
+    return cmap
+
+def sample_cmap(cmap, n):
+    """Utility to sample a Colormap
+
+    Parameters
+    ----------
+    cmap : matplotlib.colors.Colormap
+        Colormap to sample
+    n : int
+        number of colors to sample
+
+    Returns
+    -------
+    matplotlib.colors.ListedColormap
+        discrete colormap with sampled colors
+
+    """
+    if isinstance(cmap, str):
+        cmap = cm.get_cmap(cmap)
+    elif isinstance(cmap, col.Colormap):
+        pass
+    else:
+        raise TypeError('The argument "cmap" must be a string or Colormap')
+    vals = np.linspace(0,1,n)
+    cmap = col.ListedColormap(cmap(vals))
+    return cmap
