905350e0ab159cc97a96dcab57bd06c3e456369e
Alessio Arena <alessio.arena@csiro.au>
2021-11-02 10:23:10 +1000

    add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#python setup.py build_ext --inplace
+#python setup.py bdist_wheel --universal
+from setuptools import setup, find_packages
+
+requires = [
+    "matplotlib>=3.4.1",
+    "ipython>=7.22",
+    "numpy",
+    "branca>=0.4.2",
+]
+
+setup(
+    name="csiro_colors",
+    version="0.1",
+    description="CSIRO and generic colormap handler",
+    author="Alessio Arena",
+    author_email="alessio.arena@csiro.au",
+    packages = find_packages(),
+    install_requires=requires,
+)
